e9fea328b41bb2bcc36b4ed9b60ac1aa3043feb0
petroyurkiv <igorlamativ@gmail.com>
2023-05-09 20:41:55 +0300

    first safe

---
+++ SimpleApp/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleApp
+//
+//  Created by Petro on 09.05.2023.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
